f484c2acf45896a8d9640ce19c1ae78cee29f511
Oncecat <oncecat1@gmail.com>
2020-05-17 16:09:01 +0300

    added motherboard,cpu,gpu info

--- core/systinfo.py
+++ core/systinfo.py
@@ -1,33 +1,34 @@
-import platform
+import platform, os, re, socket, wmi
 import psutil as ps
 import subprocess as sb
-import os 
-import re
 from urllib.request import urlopen
-import socket
 from getmac import get_mac_address
+import cpuinfo as cp
 
-
+#current system info
 os_info = str(platform.system() + " " + platform.release())
 print()
 print("Current system is " + os_info)
 
+#total ram info
 mem = str(sb.check_output('wmic memorychip get Capacity').decode('cp866'))
 mem = mem.replace('Capacity','')
-mem = int(mem)/2**30  # перевод байты в гигабайты
+mem = int(mem)/(2**30)  # перевод байты в гигабайты
 print("Current RAM is " + str(int(mem)) + " Gb")
 
+#ram frequency
 mem_fr = str(sb.check_output('wmic memorychip get Speed').decode('cp866'))
 mem_fr = mem_fr.replace('Speed','').replace('\r\r\n','')
 print("RAM frequency is" + str(mem_fr)) # Частота оперативной памяти
 
+#hdd f/t/u
 hdd = ps.disk_usage('/')
 hdd_t = hdd.total/(2**30)
 hdd_u = hdd.used/(2**30)
 hdd_f = hdd.free/(2**30)
-print("total hdd memory: %.2f Gb "% hdd_t)
-print("used hdd memory: %.2f Gb "% hdd_u)
-print("free hdd memory: %.2f Gb "% hdd_f)
+print("free hdd memory: %.2f Gb"% hdd_f)
+print("total hdd memory: %.2f Gb"% hdd_t)
+print("used hdd memory: %.2f Gb"% hdd_u)
 
 # get white ip 
 data = str(urlopen('http://checkip.dyndns.com/').read())
@@ -42,3 +43,28 @@ print("local ip: " + IPAddr)
 # get local mac
 mac = get_mac_address()
 print("current mac-address: " + mac)
+
+#motherboard info
+motherboard_manufact = list(str(sb.check_output('wmic baseboard get manufacturer').decode('cp866').replace('Manufacturer','').replace('\r','').replace('\n','')))
+motherboard_product = list(str(sb.check_output('wmic baseboard get product').decode('cp866').replace('Product','').replace('\r','').replace('\n','')))
+while (' ' in motherboard_product):
+    motherboard_product.remove(' ')
+
+while(' ' in motherboard_manufact):
+    motherboard_manufact.remove(' ')
+
+motherboard_manufact = "".join(motherboard_manufact)
+motherboard_product = "".join(motherboard_product)
+
+print("your motherboard: " + motherboard_manufact,motherboard_product)
+
+#cpu info
+cpu_cores = ps.cpu_count()
+cpu_model = cp.get_cpu_info()
+cpu_model = cpu_model["brand"]
+print("your cpu: %s %d cores" % (cpu_model, cpu_cores))
+
+#gpu info
+pc = wmi.WMI()
+gpu_info = pc.Win32_VideoController()[0].Name
+print("your gpu: " + gpu_info)
